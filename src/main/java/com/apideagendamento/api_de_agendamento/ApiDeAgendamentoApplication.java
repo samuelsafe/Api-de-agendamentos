5da4f4ed7889b57ed78d822d94deec6198494f2e
Samuel <sf38584@gmail.com>
2026-04-28 20:15:55 -0300

    inicio da api

---
+++ src/main/java/com/apideagendamento/api_de_agendamento/ApiDeAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.apideagendamento.api_de_agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeAgendamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apideagendamento/api_de_agendamento/ApiDeAgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apideagendamento.api_de_agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeAgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
